c18901a2bbbc33df8dcbcf571188654ff163e55b
SVEN RODRIGUEZ <SVEN RODRIGUEZ@DESKTOP-LR0EK8G>
2020-05-29 09:43:26 -0600

    Se añadió el proyecto facturaSencilla

--- Validaciones/Validaciones/My Project/Application.Designer.vb
+++ Validaciones/Validaciones/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Validaciones.ComboBox_Equipos
+            Me.MainForm = Global.Validaciones.facturaSencilla
         End Sub
     End Class
 End Namespace
