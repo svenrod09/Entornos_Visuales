fc86fd0ad01ec0c710d404783e511cff94c8f534
SVEN RODRIGUEZ <SVEN RODRIGUEZ@DESKTOP-LR0EK8G>
2020-05-28 22:47:18 -0600

    Se creó el proyecto Combo Box Equipos

---
+++ Validaciones/Validaciones/ComboBox Equipos.vb
@@ -0,0 +1,25 @@
+﻿Public Class ComboBox_Equipos
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbEquipos.SelectedIndexChanged
+        Dim itemId As Integer
+        Dim itemNombre As String
+
+        itemId = cmbEquipos.SelectedIndex
+        itemNombre = cmbEquipos.SelectedItem.ToString
+        Select Case itemId
+            Case 0
+                MsgBox(itemNombre)
+            Case 1
+                MsgBox(itemNombre)
+            Case 2
+                MsgBox(itemNombre)
+            Case 3
+                MsgBox(itemNombre)
+            Case 4
+                MsgBox(itemNombre)
+            Case 5
+                MsgBox(itemNombre)
+            Case Else
+                MsgBox("Selecciona un valor correcto")
+        End Select
+    End Sub
+End Class
