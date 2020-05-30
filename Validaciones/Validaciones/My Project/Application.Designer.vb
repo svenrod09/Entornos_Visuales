92706a13b0bfcaa5f52022db65f8f593278a3611
SVEN RODRIGUEZ <SVEN RODRIGUEZ@DESKTOP-LR0EK8G>
2020-05-30 16:41:45 -0600

    Se agregó el proyecto cicloFor

--- Validaciones/Validaciones/My Project/Application.Designer.vb
+++ Validaciones/Validaciones/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Validaciones.operacionesConPotenciaYRaiz
+            Me.MainForm = Global.Validaciones.cicloFor
         End Sub
     End Class
 End Namespace
